cd35c40b684504f1aff1e6ae1ba5cddeee6b884c
med134 <81263260+med134@users.noreply.github.com>
2025-02-24 17:58:54 +0000

    add controller

--- src/api/article/controllers/article.ts
+++ src/api/article/controllers/article.ts
@@ -1,7 +1,23 @@
-/**
- *  article controller
- */
+import { factories } from "@strapi/strapi";
 
-import { factories } from '@strapi/strapi';
+export default factories.createCoreController(
+  "api::article.article",
+  ({ strapi }) => ({
+    async findOne(ctx) {
+      const { id } = ctx.params;
 
-export default factories.createCoreController('api::article.article');
+      const entity = await strapi.db.query("api::article.article").findOne({
+        where: { slug: id },
+        populate: ["image"],
+      });
+
+      if (!entity) {
+        return ctx.notFound("Article not found");
+      }
+
+      const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+      return this.transformResponse(sanitizedEntity);
+    },
+  })
+);
