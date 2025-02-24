b7f8f773f03aff7fe3a8b47081a7e3b407dfedee
med134 <81263260+med134@users.noreply.github.com>
2025-02-24 22:33:26 +0000

    add cont

--- src/api/author/controllers/author.ts
+++ src/api/author/controllers/author.ts
@@ -1,7 +1,23 @@
-/**
- *  author controller
- */
+import { factories } from "@strapi/strapi";
 
-import { factories } from '@strapi/strapi';
+export default factories.createCoreController(
+  "api::author.author",
+  ({ strapi }) => ({
+    async findOne(ctx) {
+      const { id } = ctx.params;
 
-export default factories.createCoreController('api::author.author');
+      const entity = await strapi.db.query("api::author.author").findOne({
+        where: { id },
+        populate: ["avatar"],
+      });
+
+      if (!entity) {
+        return ctx.notFound("Article not found");
+      }
+
+      const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+      return this.transformResponse(sanitizedEntity);
+    },
+  })
+);
